b31dd341f417e1bc90377d266bf98533c14ef863
rounakbharti <167201531+rounakbharti@users.noreply.github.com>
2025-03-28 20:12:40 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://tinylane-73t2.onrender.com' || '/api/';
+const API_BASE_URL = 'https://tinylane-73t2.onrender.com/api' || '/api/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
