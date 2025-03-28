9cc91118ad98e96fef42155b54298de52f76f441
rounakbharti <167201531+rounakbharti@users.noreply.github.com>
2025-03-28 19:48:20 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://tinylane-73t2.onrender.com',
         changeOrigin: true,
       },
     },
